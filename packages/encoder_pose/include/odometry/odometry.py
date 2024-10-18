15b57305a37ab5c289301a71c96bc926e3cb1524
hugo <hugo@baudchon.com>
2024-10-17 22:02:53 -0400

    my changes

--- packages/encoder_pose/include/odometry/odometry.py
+++ packages/encoder_pose/include/odometry/odometry.py
@@ -14,10 +14,10 @@ def delta_phi(ticks: int, prev_ticks: int, resolution: int) -> Tuple[float, floa
         ticks: current number of ticks.
     """
 
-    # TODO: these are random values, you have to implement your own solution in here
-    ticks = prev_ticks + int(np.random.uniform(0, 10))
-    dphi = np.random.random()
-    # ---
+    alpha = 2 * np.pi / resolution
+    delta_ticks = ticks - prev_ticks
+    dphi = alpha * delta_ticks
+
     return dphi, ticks
 
 
@@ -50,9 +50,39 @@ def estimate_pose(
         theta_curr:              estimated heading
     """
 
-    # These are random values, replace with your own
-    x_curr = np.random.random()
-    y_curr = np.random.random()
-    theta_curr = np.random.random()
-    # ---
+    d_left = R * delta_phi_left
+    d_right = R * delta_phi_right
+    d_A = (d_left + d_right) / 2
+    d_theta = (d_right - d_left) / baseline
+
+    t_prev_pos = np.array([
+        [np.cos(theta_prev), -np.sin(theta_prev), x_prev],
+        [np.sin(theta_prev), np.cos(theta_prev), y_prev],
+        [0, 0, 1]
+    ])
+
+    t_new_pos = ([
+        [np.cos(d_theta), -np.sin(d_theta), d_A * np.cos(d_theta)],
+        [np.sin(d_theta), np.cos(d_theta), d_A * np.sin(d_theta)],
+        [0, 0, 1]
+    ])
+
+    new_pos = np.dot(t_prev_pos, t_new_pos)
+
+    x_curr = new_pos[0, 2]
+    y_curr = new_pos[1, 2]
+    theta_curr = theta_prev + d_theta
+
     return x_curr, y_curr, theta_curr
+
+
+if __name__ == "__main__":
+    R = 0.0318
+    baseline = 0.1
+    x_prev = 0
+    y_prev = 0
+    theta_prev = 0
+    delta_phi_right = 100
+    delta_phi_left = 100
+
+    print(estimate_pose(R, baseline, x_prev, y_prev, theta_prev, delta_phi_left, delta_phi_right))
